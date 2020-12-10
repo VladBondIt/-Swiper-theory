082319e90fc34a100cf32abfccbed39504c8faff
Vlad Bond <foolowin@gmail.com>
2020-12-10 13:56:01 +0300

    tbc

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,4 +1,4 @@
-new Swiper('.image-slider', {
+const mySwiper = new Swiper('.image-slider', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
@@ -11,12 +11,17 @@ new Swiper('.image-slider', {
         // renderBullet: function (index, className) {
         //     return '<span class="' + className + '">' + (index + 1) + '</span>';
         // },
-        // type: 'fraction',
-        // renderFraction: function (currentClass, totalClass) {
-        //     return 'Изображение <span class="' + currentClass + '"></span>' +
-        //         ' из ' +
-        //         '<span class="' + totalClass + '"></span>';
-        // },
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+            return 'Изображение <span class="' + currentClass + '"></span>' +
+                ' из ' +
+                '<span class="' + totalClass + '"></span>';
+        },
     },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        dragClass: 'swiper-scrollbar-drag'
+    }
 
 });
