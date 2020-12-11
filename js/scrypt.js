653fa78dde74633ee86672c430f1ebcb83a65f93
Vlad Bond <foolowin@gmail.com>
2020-12-11 12:50:08 +0300

    tbc

--- js/scrypt.js
+++ js/scrypt.js
@@ -3,26 +3,27 @@ const mySwiper = new Swiper('.image-slider', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
-    pagination: {
-        el: '.swiper-pagination',
-        // clickable: true,
-        // // dynamicBullets неочень
-        // // dynamicBullets: true,
-        // renderBullet: function (index, className) {
-        //     return '<span class="' + className + '">' + (index + 1) + '</span>';
-        // },
-        type: 'fraction',
-        renderFraction: function (currentClass, totalClass) {
-            return 'Изображение <span class="' + currentClass + '"></span>' +
-                ' из ' +
-                '<span class="' + totalClass + '"></span>';
-        },
-    },
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-        dragClass: 'swiper-scrollbar-drag'
-    },
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     // clickable: true,
+    //     // type: 'bullets',
+    //     // // dynamicBullets неочень
+    //     // // dynamicBullets: true,
+    //     // renderBullet: function (index, className) {
+    //     //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+    //     // },
+    //     type: 'fraction',
+    //     renderFraction: function (currentClass, totalClass) {
+    //         return 'Изображение <span class="' + currentClass + '"></span>' +
+    //             ' из ' +
+    //             '<span class="' + totalClass + '"></span>';
+    //     },
+    // },
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //     draggable: true,
+    //     dragClass: 'swiper-scrollbar-drag'
+    // },
     keyboard: {
         enabled: true,
         onlyInViewport: true,
@@ -33,18 +34,18 @@ const mySwiper = new Swiper('.image-slider', {
         eventsTarget: '.image-slider'
     },
     autoHeight: true,
-    slidesPerView: 1,
+    slidesPerView: 3,
     // Авто указывается только в том случае если конкретно указаны размеры сладов
     // slidesPerView: auto,
     // slidesPerView: 2.5,
     spaceBetween: 20,
 
-    slidesPerGroup: 1,
+    // slidesPerGroup: 1,
 
     // centeredSlides: true,
 
     // Стартовый слайд c Нуля
-    initialSlide: 1,
+    // initialSlide: 1,
 
     // slidesPerColumn: 2,
 
@@ -90,23 +91,23 @@ const mySwiper = new Swiper('.image-slider', {
     //     stretch: 50,
     //     slideShadows: true
     // }
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
-        // when window width is >= 480px
-        480: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        // when window width is >= 992px
-        992: {
-            slidesPerView: 2,
-            spaceBetween: 40
-        }
-    },
+    // breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 30
+    //     },
+    //     // when window width is >= 992px
+    //     992: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 40
+    //     }
+    // },
 
 
     preloadImages: false,
@@ -120,4 +121,45 @@ const mySwiper = new Swiper('.image-slider', {
     watchSlidesProgress: true,
     // Добавление класса видимым слайдам
     watchSlidesVisibility: true,
+
+
+    // Для зума нужны плагины
+    // zoom: {
+    //     maxRatio: 5,
+
+    //     minRatio: 1
+    // }
+
+
+    // Миниатюры(превью)
+    // thumbs: {
+    //     swiper: {
+    //         el: '.image-mini-slider',
+    //         slidesPerView: 5,
+    //         mousewheel: {
+    //             sensitivity: 1,
+    //             eventsTarget: '.image-mini-slider'
+    //         },
+    //         spaceBetween: 7,
+    //     },
+    // }
 });
+
+// Инициализируем новый слайдер в слайдере
+// const inSlider = new Swiper('.image-in-slider', {
+//     grabCursor: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//     },
+//     // Отвязка от родительского слайдра указываем вложженость
+//     nested: true,
+
+// });
+
+
+const textSwiper = new Swiper('.text-slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+});
+
